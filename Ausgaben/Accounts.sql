9a2ac206b5b478ee59702b42833acf9ac739a7c7
Dmytro Morozov <dmytro.morozov@check24.de>
2015-12-29 12:07:59 +0100

    Initial project commit

---
+++ Ausgaben/Accounts.sql
@@ -0,0 +1,14 @@
+﻿/*
+The database must have a MEMORY_OPTIMIZED_DATA filegroup
+before the memory optimized object can be created.
+
+The bucket count should be set to about two times the 
+maximum expected number of distinct values in the 
+index key, rounded up to the nearest power of two.
+*/
+
+CREATE TABLE [dbo].[Accounts]
+(
+	[Id] BIGINT NOT NULL IDENTITY(1,1),
+	CONSTRAINT [PK_Id] PRIMARY KEY NONCLUSTERED (Id ASC)
+)
