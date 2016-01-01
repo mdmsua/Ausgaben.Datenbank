9348f10dc46a3dfb12299ea288bdf6d210ccc510
Dmytro Morozov <mdmsua@icloud.com>
2016-01-01 18:59:23 +0100

    Add Payments table

---
+++ Ausgaben/dbo.Payments.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Payments]
+(
+	[Id] BIGINT NOT NULL IDENTITY(1,1) CONSTRAINT [PK_Payments_Id] PRIMARY KEY,
+	[AccountId] INT NOT NULL CONSTRAINT [FK_Payments_AccountId_Accounts] FOREIGN KEY REFERENCES dbo.Accounts(Id),
+	[CategoryId] INT NOT NULL CONSTRAINT [FK_Payments_CategoryId_Categories] FOREIGN KEY REFERENCES dbo.Categories(Id),
+	[Amount] DECIMAL(8,2) NOT NULL CONSTRAINT [CK_Payments_Amount] CHECK (Amount <> 0),
+	[Timestamp] DATETIMEOFFSET NOT NULL CONSTRAINT [DF_Payments_Day] DEFAULT SYSDATETIMEOFFSET()
+)
