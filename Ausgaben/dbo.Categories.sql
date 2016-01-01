73d7ffd475e1c7998018b7d959c102fd63b5398a
Dmytro Morozov <mdmsua@icloud.com>
2016-01-01 18:31:18 +0100

    Add Type column to Categories table

--- Ausgaben/dbo.Categories.sql
+++ Ausgaben/dbo.Categories.sql
@@ -2,6 +2,7 @@
 (
 	[Id] INT NOT NULL IDENTITY(1,1),
 	[Name] NVARCHAR(128) NOT NULL,
+	[Type] BIT NOT NULL, -- 0: expense, 1: income
 	CONSTRAINT [PK_Categories_Id] PRIMARY KEY NONCLUSTERED (Id ASC),
 	CONSTRAINT [CK_Categories_Name] CHECK (LEN(Name) > 0)
 )
