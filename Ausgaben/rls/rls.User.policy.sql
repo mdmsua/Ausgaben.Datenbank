c39baa469cc649289a69f02edee846a9a573900a
Dmytro Morozov <mdmsua@icloud.com>
2016-01-03 23:59:35 +0100

    Implement row-level security for Accounts table

---
+++ Ausgaben/rls/rls.User.policy.sql
@@ -0,0 +1,3 @@
+﻿CREATE SECURITY POLICY [rls].[User]
+    ADD FILTER PREDICATE [rls].[fn_securitypredicate]([User]) ON [dbo].[Accounts]
+    WITH (STATE = ON)
---
+++ Ausgaben/rls/rls.fn_securitypredicate.function.sql
@@ -0,0 +1,8 @@
+﻿CREATE FUNCTION [rls].[fn_securitypredicate]
+(
+	@User	NVARCHAR(128)
+)
+RETURNS TABLE WITH SCHEMABINDING
+AS
+	RETURN SELECT 1 AS fn_securitypredicate_result
+    WHERE CAST(SESSION_CONTEXT(N'User') AS NVARCHAR(128)) = @User
